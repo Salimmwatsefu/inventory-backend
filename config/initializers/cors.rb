b4839aeb4ab57e4a44bbe5d34cba8a50ed4f2b0c
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2023-08-08 09:54:05 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000', 'https://kuku-hub.vercel.app/' # Add your frontend's origin here
+      origins 'http://localhost:3000', 'https://kuku-hub.vercel.app' # Add your frontend's origin here
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], expose: ['Authorization'], credentials: true
     end
-  end
+  end 
